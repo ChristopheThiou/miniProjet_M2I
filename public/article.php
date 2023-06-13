113abda806a64fbbca080a7a50fdc6022d8f3a25
ChristopheThiou <christophe.thiou.simplon@gmail.com>
2023-06-13 14:24:20 +0200

    Final !

--- public/article.php
+++ public/article.php
@@ -45,7 +45,7 @@ $comments = $repositoryCo->findByCom($_GET["id"])
 
 <body class="container-fluid">
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <a class="navbar-brand" href="index.php"> <img id="logo"
                 src="https://cdn.pixabay.com/photo/2019/06/27/21/14/logo-4303138_1280.png" alt="oui"> </a>
     </nav>
